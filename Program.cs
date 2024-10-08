08dde54d87fd456f77c15e96d42ff7841ef6ba58
Isaac R <isaac.rebolledoleal@gmail.com>
2024-10-08 09:12:33 -0300

    optimizing code and uploading all earthquakes until 08/10/2024

--- Program.cs
+++ Program.cs
@@ -2,26 +2,33 @@
 
 Console.WriteLine("Running... on " + DateTimeOffset.Now);
 
-List<Earthquake> earthquakes = new List<Earthquake>();
+List<Earthquake> earthquakes = [];
 
-var dateLimit = new DateTimeOffset(2000, 2, 1, 0, 0, 0, TimeSpan.Zero);
-
-for (DateTimeOffset date = new DateTimeOffset(2000, 1, 1, 0, 0, 0, TimeSpan.Zero); date <= DateTimeOffset.Now; date = date.AddDays(1))
-// for (DateTimeOffset date = new DateTimeOffset(2000, 1, 1, 0, 0, 0, TimeSpan.Zero); date <= dateLimit; date = date.AddDays(1))
+for (int year = 2000; year <= DateTimeOffset.Now.Year; year++)
 {
-    Console.WriteLine($"Getting earthquakes for {date}...");
+    Parallel.For(1, 13, month =>
+    {
+        var date = new DateTimeOffset(year, month, 1, 0, 0, 0, TimeSpan.Zero);
+        Console.WriteLine($"Getting earthquakes for {date}...");
 
-    try
-    {
-        var result = SismicCrawler.GetEarthquakes(date);
-        earthquakes.AddRange(result);
-        Console.WriteLine($"{date} ready");
-    }
-    catch
-    {
-        Console.WriteLine($"Failed to get earthquakes for {date}");
-    }
+        try
+        {
+            var result = SismicCrawler.GetEarthquakes(date);
+            lock (earthquakes)
+            {
+                earthquakes.AddRange(result);
+            }
+            Console.WriteLine($"{date} ready");
+        }
+        catch
+        {
+            Console.WriteLine($"Failed to get earthquakes for {date}");
+        }
+    });
+
+    Console.WriteLine($"Done for {year}");
 }
 
-// Save earthquakes to file as json using system.text.json
+
 File.WriteAllText("earthquakes.json", JsonSerializer.Serialize(earthquakes));
+Console.WriteLine("Done!");
