3617ba18e948dfd63b3cc8f958b9bd9d75de11f7
Isaac R <isaac.rebolledoleal@gmail.com>
2024-10-14 19:04:24 -0300

    updating code an json file

--- Program.cs
+++ Program.cs
@@ -8,27 +8,31 @@ for (int year = 2000; year <= DateTimeOffset.Now.Year; year++)
 {
     Parallel.For(1, 13, month =>
     {
-        var date = new DateTimeOffset(year, month, 1, 0, 0, 0, TimeSpan.Zero);
-        Console.WriteLine($"Getting earthquakes for {date}...");
+        Parallel.For(1, DateTime.DaysInMonth(year, month) + 1, day =>
+        {
+            var date = new DateTimeOffset(year, month, day, 0, 0, 0, TimeSpan.Zero);
+            Console.WriteLine($"Getting earthquakes for {date}...");
 
-        try
-        {
-            var result = SismicCrawler.GetEarthquakes(date);
-            lock (earthquakes)
+            try
             {
-                earthquakes.AddRange(result);
+                var result = SismicCrawler.GetEarthquakes(date);
+                lock (earthquakes)
+                {
+                    earthquakes.AddRange(result);
+                }
+                Console.WriteLine($"{date} ready");
             }
-            Console.WriteLine($"{date} ready");
-        }
-        catch
-        {
-            Console.WriteLine($"Failed to get earthquakes for {date}");
-        }
+            catch
+            {
+                Console.WriteLine($"Failed to get earthquakes for {date}");
+            }
+        });
     });
 
     Console.WriteLine($"Done for {year}");
 }
 
+earthquakes.Sort((a, b) => a.Date.CompareTo(b.Date));
 
 File.WriteAllText("earthquakes.json", JsonSerializer.Serialize(earthquakes));
 Console.WriteLine("Done!");
